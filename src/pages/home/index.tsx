18d56c5dea37bdb0c2763dc7f0fcf5a24928d1fa
chenxiaolani <2524729719@qq.com>
2021-08-12 20:20:11 +0800

    feat: add home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,14 +1,33 @@
 import { Button } from 'antd'
-import React, { memo } from 'react'
+import React, { memo, useState } from 'react'
+import './home.scss'
 
 export const Home = memo(() => {
+  const [fullScreen, setFullScreen] = useState<boolean>(false)
+
   const handleOnLoginOut = () => {
     console.log('退出登录')
   }
 
+  const handleFullPage = () => {
+    const wrapper = document.getElementsByClassName('home')[0]
+    console.log('home', wrapper)
+    setFullScreen(true)
+    wrapper.requestFullscreen()
+  }
+
   return (
-    <div>
-      这是主页<Button onClick={handleOnLoginOut}>loginOut</Button>
+    <div className="home">
+      {!fullScreen && <div className="home_left">左边的缩略图</div>}
+
+      <div className="home_right">
+        <iframe
+          src={
+            'https://ppt2h5.qcloudtiw.com/0868sh7lgsh31nfem94c/index.html?page=3&step=12&fid=%231628662412037&originUrl=https%3A%2F%2Fppt2h5.qcloudtiw.com%2F0868sh7lgsh31nfem94c%2Findex.html'
+          }
+        />
+      </div>
+      {!fullScreen && <Button onClick={handleFullPage}>全屏</Button>}
     </div>
   )
 })
