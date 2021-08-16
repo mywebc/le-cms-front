9c6360e7ef7ae566bc77b78e63ad3716a3cef7fa
chenxiaolani <2524729719@qq.com>
2021-08-16 15:19:23 +0800

    feat: add layout component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { useRoutes } from 'react-router-dom'
+import { LeLayout } from '../components/Layout'
 import { RouteListType } from '../interface/routes'
 import { Login } from '../pages/auth/login'
 import { Home } from '../pages/home'
 
 const routeList: RouteListType = [
+  {
+    path: '/',
+    element: <LeLayout />
+  },
   {
     path: '/auth',
     element: <Login />
